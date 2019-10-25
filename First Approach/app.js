e76aef399cb5ab2e3d80eee82f486cfc1d14c4c6
Gastón De los Reyes <mu_gas@hotmail.com>
2019-10-25 11:47:03 -0300

    Probando funciones básica de JavaScript

---
+++ First Approach/app.js
@@ -0,0 +1,10 @@
+document.write("<h2>Hello again World, Now I'm writing from a JavaScript file</h2>");
+
+//Arrays [value,value,value]
+//Objects {"attribute": Value}
+username = "Gastón";
+console.log({
+    "user": username,
+    "Proffesional": true,
+    "Score": 77.32
+})
